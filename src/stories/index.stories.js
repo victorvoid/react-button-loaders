35d12932ca9b88c88e7ce5b80fb26c05b330288d
Victor Igor <victor.0w3@hotmail.com>
2018-05-22 21:35:08 -0300

    adding stories to button > ProgressBar with text

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -3,34 +3,50 @@ import { storiesOf } from '@storybook/react';
 import { withState } from '@dump247/storybook-state';
 import ButtonProgressBar from '../lib/components/ButtonProgressBar'
 
-export default ButtonProgressBar
+const handleClick = (store) => {
+    store.set({ btnState: 'loading' })
+    setTimeout(() => {
+        store.set({ btnState: 'finished'})
+        setTimeout(() => store.set({ btnState: ''}), 3000)
+    }, 3000)
+}
 
-storiesOf('Button/ProgressBar', module)
-    .add('default state', () =>
-         <ButtonProgressBar>
-             Save Changes
-         </ButtonProgressBar>
-    )
-    .add('loading state', () =>
+storiesOf('Buttons/ProgressBar', module)
+    .add('default', withState({ btnState: '' })(({ store }) => (
+        <ButtonProgressBar
+            state={store.state.btnState}
+            onClick={() => handleClick(store)}>
+            Save Changes
+        </ButtonProgressBar>
+    )))
+    .add('default with text', withState({ btnState: '' })(({ store }) => (
+        <ButtonProgressBar
+            textWhileLoading='Saving'
+            textWhenLoaded ='Saved'
+            state={store.state.btnState}
+            onClick={() => handleClick(store)}>
+            Save Changes
+        </ButtonProgressBar>
+    )))
+    .add('loading', () =>
          <ButtonProgressBar state='loading'>
              Save Changes
          </ButtonProgressBar>
 
     )
-    .add('loading completed', () =>
+    .add('loading with text', () =>
+         <ButtonProgressBar state='loading' textWhileLoading='Saving'>
+             Save Changes
+         </ButtonProgressBar>
+
+    )
+    .add('completed', () =>
          <ButtonProgressBar state='finished'>
              Save Changes
          </ButtonProgressBar>
     )
-    .add('example clickable', withState({ btnState: '' })(({ store }) => (
-        <ButtonProgressBar
-            state={store.state.btnState}
-            onClick={() => {
-                    store.set({ btnState: 'loading' })
-                    setTimeout(() => {
-                        store.set({ btnState: 'finished'})
-                    }, 3000)
-                }}>
-            Save Changes
-        </ButtonProgressBar>
-    )))
+    .add('completed with text', () =>
+         <ButtonProgressBar state='finished' textWhenLoaded='Saved'>
+             Save Changes
+         </ButtonProgressBar>
+    )
