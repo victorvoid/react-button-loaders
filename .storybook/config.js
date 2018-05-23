f282eb0ff035b7c24d4c15f639b7304f884f886f
Victor Igor <victor.0w3@hotmail.com>
2018-05-22 21:12:37 -0300

    improving storybook and remove demo code

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,13 @@
 import { configure } from '@storybook/react';
+import { injectGlobal } from 'styled-components';
 
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Heebo:400,600');
+  button span {
+    font-family: 'Heebo', sans-serif;
+    font-weight: 600;
+  }
+`
 // automatically import all files ending in *.stories.js
 const req = require.context('../src/stories', true, /.stories.js$/);
 function loadStories() {
--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import ButtonProgressBar from '../demo/ButtonProgressBar'
+import { withState } from '@dump247/storybook-state';
+import ButtonProgressBar from '../lib/components/ButtonProgressBar'
 
-storiesOf('Button', module)
-  .add('ButtonProgressBar', () => <ButtonProgressBar/>)
+export default ButtonProgressBar
+
+storiesOf('Button/ProgressBar', module)
+    .add('default state', () =>
+         <ButtonProgressBar>
+             Save Changes
+         </ButtonProgressBar>
+    )
+    .add('loading state', () =>
+         <ButtonProgressBar state='loading'>
+             Save Changes
+         </ButtonProgressBar>
+
+    )
+    .add('loading completed', () =>
+         <ButtonProgressBar state='finished'>
+             Save Changes
+         </ButtonProgressBar>
+    )
+    .add('example clickable', withState({ btnState: '' })(({ store }) => (
+        <ButtonProgressBar
+            state={store.state.btnState}
+            onClick={() => {
+                    store.set({ btnState: 'loading' })
+                    setTimeout(() => {
+                        store.set({ btnState: 'finished'})
+                    }, 3000)
+                }}>
+            Save Changes
+        </ButtonProgressBar>
+    )))
